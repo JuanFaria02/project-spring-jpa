32f2cb543c8f00e30ec506a80f689028722fa034
Juan Faria <juanfariacrf@gmail.com>
2023-01-04 15:11:34 -0300

    Exception of findById create

--- src/main/java/com/projectspring/springproject/services/UserServices.java
+++ src/main/java/com/projectspring/springproject/services/UserServices.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projectspring.springproject.entities.User;
 import com.projectspring.springproject.repositories.UserRepository;
+import com.projectspring.springproject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserServices {
@@ -22,7 +23,7 @@ public class UserServices {
 	public User findById(Long id) {
 		
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
